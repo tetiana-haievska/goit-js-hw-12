4428ea2fe914dc34c66309e6fc37883e855ba165
tetiana-haievska <tatiana.makarenko94@gmail.com>
2024-11-27 13:37:54 +0000

    Deploying to gh-pages from @ tetiana-haievska/goit-js-hw-10@cdd8d6cc45421b18ba702636226c8bb71dd60a36 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i as r}from"./assets/vendor-BbbuE1sJ.js";const m=document.querySelector(".form");m.addEventListener("submit",t=>{t.preventDefault();const s=parseInt(t.target.elements.delay.value),i=t.target.elements.state.value;new Promise((e,o)=>{setTimeout(()=>{i==="fulfilled"?e(s):o(s)},s)}).then(e=>{r.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{r.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=2-snackbar.js.map
